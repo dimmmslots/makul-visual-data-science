a1620135a25baf80ea7d5204879281ab6cab8dd0
dimmmslots <vctrline2048@gmail.com>
2022-10-05 18:05:37 +0700

    editing segitiga.py

--- tugas2/segitiga.py
+++ tugas2/segitiga.py
@@ -1,9 +1,7 @@
 line_counter = 1
-star_counter_src = 1
 while line_counter <= 5:
-    for i in range(star_counter_src):
+    for i in range(line_counter):
         print("*", end="")
     print()
     line_counter += 1
-    star_counter_src += 1
     
