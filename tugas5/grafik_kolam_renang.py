bc67288e42a7aeaec72bee9ff9a65f6af3b1237e
dimmmslots <vctrline2048@gmail.com>
2022-11-02 10:19:05 +0700

    menyelesaikan tugas 5

---
+++ tugas5/grafik_kolam_renang.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as grafik
+import numpy as np
+
+data_pengunjung = np.array([3204,560,2900,2140,6679,3000,200,0,0,0,0,3518])
+
+bulan =np.char.array(['Jan','Feb','Mar','Apr','Mei','Jun','Jul','Agu','Sep','Okt','Nov','Des'])
+
+grafik.plot(bulan, data_pengunjung)
+# anotasi di setiap titik
+for a,b in zip(bulan, data_pengunjung): 
+    grafik.text(a, b, str(b))
+grafik.show()
+
+grafik.bar(bulan, data_pengunjung)
+# anotasi di atas tiap bar
+for a,b in zip(bulan, data_pengunjung): 
+    if b > 0:
+        grafik.text(a, b, str(b))
+grafik.show()
+
+# menghitung persentase tiap data
+porcent = 100.*data_pengunjung/data_pengunjung.sum()
+patches = grafik.pie(data_pengunjung, shadow=True, startangle=90)[0]
+# store the return value from pie() in a variable
+
+labels = ['{0} - {1:.2f} %'.format(bulan_val,porcent_val) for bulan_val,porcent_val in zip(bulan, porcent)]
+grafik.legend(patches, labels, loc='best', bbox_to_anchor=(-0.1, 1.),
+fontsize=8)
+
+grafik.show()
